43c39837916032382db99a66d1203393513bd749
valeriapopova <iamvaleriapopova@yandex.ru>
2022-08-27 14:42:10 +0400

    new options for update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
+from werkzeug.exceptions import BadRequestKeyError
+
 from config import Configuration
 from forms import FileForm
 from sheets import *
@@ -18,31 +20,38 @@ def post():
 
 
 @app.errorhandler(404)
-def page_not_found(event):
+def page_not_found():
     return render_template('404.html'), 404
 
 
 @app.route('/json', methods=['POST', 'GET'])
 def post_json_to_google_sheets():
     if request.method == 'POST':
-        json_file = request.form['file']
-        if json_file.endswith('.json'):
-            service = get_service(json_file)
-            spreadsheet_id = get_spreadsheet(json_file)
-            data_keys = get_data_key(json_file)
-            data_values = get_data_value(json_file)
+        try:
+            json_file = request.form['file']
+            if json_file.endswith('.json'):
+                service = get_service(json_file)
+                spreadsheet_id = get_spreadsheet(json_file)
+                data_keys = get_data_key(json_file)
+                data_values = get_data_value(json_file)
 
-            values = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range='Лист1!A1',
-                                                            valueInputOption="RAW",
-                                                            body={'values': data_keys}).execute()
+                if request.form['submit_button'] == 'Дописать данные(названия колонок и значения)':
+                    append(service, spreadsheet_id, data_keys, data_values)
+                    return redirect('/post', 301)
+                elif request.form['submit_button'] == 'Дописать только значения':
+                    append_values(service, spreadsheet_id, data_values)
+                    return redirect('/post', 301)
+                elif request.form['submit_button'] == 'Перезаписать':
+                    clear_and_append(service, spreadsheet_id, data_keys, data_values)
+                    return redirect('/post', 301)
+                elif request.form['submit_button'] == 'Записать в новый лист':
+                    append_new_list(service, spreadsheet_id, data_keys, data_values)
+                    return redirect('/post', 301)
 
-            values = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range='Лист1!A1',
-                                                            valueInputOption="RAW",
-                                                            body={"majorDimension": "COLUMNS",
-                                                                  'values': data_values}).execute()
-            return redirect('/post', 301)
-        else:
-            return Response("Недопустимый формат файла, необходимое расширение - JSON", 404)
+            else:
+                return Response("Недопустимый формат файла, необходимое расширение - JSON", 404)
+        except BadRequestKeyError:
+            return Response("Пустое значение", 400)
 
     form = FileForm()
     return render_template('post_json.html', form=form), 200
--- sheets.py
+++ sheets.py
@@ -1,16 +1,19 @@
+import random
 import json
+
 import apiclient
 import httplib2
 from flask import Response
+from googleapiclient.errors import HttpError
 from oauth2client.service_account import ServiceAccountCredentials
 
 
 def get_service(json_file):
     try:
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        json_file,
-        ['https://www.googleapis.com/auth/spreadsheets',
-         'https://www.googleapis.com/auth/drive'])
+            json_file,
+            ['https://www.googleapis.com/auth/spreadsheets',
+             'https://www.googleapis.com/auth/drive'])
         httpAuth = credentials.authorize(httplib2.Http())
         service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
     except Exception:
@@ -56,15 +59,73 @@ def get_data_value(json_file):
             return Response("Данные для записи не найдены", 404)
 
 
-
-# k = get_service('q.json')
-# # t = get_data_value('q.json')
-# #
-# print(k)
-
-
-
-
-
+def append_values(service, spreadsheet_id, data_values):
+    try:
+        values = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range='Лист1!A1',
+                                                    valueInputOption="RAW",
+                                                    body={"majorDimension": "COLUMNS",
+                                                          'values': data_values}).execute()
+    except HttpError as error:
+        return Response(f"An error occurred: {error}")
 
 
+def append(service, spreadsheet_id, data_keys, data_values):
+    try:
+        values = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range='Лист1!A1',
+                                                        valueInputOption="RAW",
+                                                        body={'values': data_keys}).execute()
+
+        values = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range='Лист1!A1',
+                                                        valueInputOption="RAW",
+                                                        body={"majorDimension": "COLUMNS",
+                                                              'values': data_values}).execute()
+    except HttpError as error:
+        return Response(f"An error occurred: {error}")
+
+
+def clear_and_append(service, spreadsheet_id, data_keys, data_values):
+    try:
+        values = service.spreadsheets().values().batchClear(spreadsheetId=spreadsheet_id,
+                                                            body={'ranges': 'A1:Z'}).execute()
+
+        values = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet_id,
+                                                             body={'valueInputOption': "USER_ENTERED",
+                                                                   'data': [{'range': 'Лист1!A1',
+                                                                             "majorDimension": "ROWS",
+                                                                             "values": data_keys},
+                                                                            {'range': 'Лист1!A2',
+                                                                             "majorDimension": "COLUMNS",
+                                                                             "values": data_values}]}).execute()
+    except HttpError as error:
+        return Response(f"An error occurred: {error}")
+
+
+def append_new_list(service, spreadsheet_id, data_keys, data_values):
+    try:
+        current_sheet = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+        if current_sheet is not None:
+            random_integer = random.randint(1, 2147483647)
+            new_sheet = {'requests': [
+                {'addSheet': {
+                    'properties': {
+                        "sheetId": random_integer,
+                        "title": '' + str(random_integer),
+                    },
+
+                }},
+
+            ]}
+            new_sheet_title = new_sheet['requests'][0]['addSheet']['properties']['title']
+            values = service.spreadsheets().batchUpdate(
+                spreadsheetId=spreadsheet_id, body=new_sheet).execute()
+
+            values = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range=f'{new_sheet_title}!A1',
+                                                            valueInputOption="RAW",
+                                                            body={'values': data_keys}).execute()
+
+            values = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range=f'{new_sheet_title}!A1',
+                                                            valueInputOption="RAW",
+                                                            body={"majorDimension": "COLUMNS",
+                                                                  'values': data_values}).execute()
+    except HttpError as error:
+        return Response(f"An error occurred: {error}")
